ca5f0675f522093d56a2bbd161dccce7fa38a3dc
Dave Syer <dsyer@pivotal.io>
2016-06-29 15:12:59 -0700

    Implement a workaround via text encryptor locator
    The bootstrap context needs a TextEncryptor that is "locator aware"
    so that it can use the cipher text to first locate an encryptor
    and then use it. The EnvironmentPrefixHelper from Spring
    Cloud is really useful here, but it is package private, so we
    hacked this workaround into the same package. A better solution
    would be to copy the code into this project.

---
+++ src/main/java/org/springframework/cloud/config/server/encryption/EncryptorLocatorBootstrapConfiguration.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2012-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.config.server.encryption;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.encrypt.TextEncryptor;
+
+/**
+ * @author Dave Syer
+ *
+ */
+@Configuration
+public class EncryptorLocatorBootstrapConfiguration implements BeanPostProcessor {
+	
+	@Autowired
+	private BeanFactory beanFactory;
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName)
+			throws BeansException {
+		return bean;
+	}
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName)
+			throws BeansException {
+		if (bean instanceof TextEncryptor && !(bean instanceof LocatorTextEncryptor)) {
+			return new LocatorTextEncryptor(beanFactory);
+		}
+		return bean;
+	}
+	
+}
